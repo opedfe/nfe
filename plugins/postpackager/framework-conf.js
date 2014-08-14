29949120460759affa50afd513bc29f63d3e0461
lifayu <lifayu@baidu.com>
2014-08-14 15:18:05 +0800

    nfe

--- plugins/postpackager/framework-conf.js
+++ plugins/postpackager/framework-conf.js
@@ -4,19 +4,19 @@ module.exports = function(ret, conf, settings, opt){
 
 	//var map = fis.config.get('framework', {});
 	//
-	var fis_sea_conf = fis.config.get('seajs', {});
-	fis_sea_conf.alias = fis_sea_conf.alias || {};
+	var fis_nfejs_conf = fis.config.get('nfejs', {});
+	fis_nfejs_conf.alias = fis_nfejs_conf.alias || {};
 	fis.util.map(ret.map.res, function(id, res){
-		fis_sea_conf.alias[id] = res.uri;
+		fis_nfejs_conf.alias[id] = res.uri;
 	});
-	//构造seajs的config.js配置文件
-	var seajs_config = fis.file(fis.project.getProjectPath(), 'sea-config.js');
-	//拼接字符串，生成sea.config调用
-	seajs_config.setContent('seajs.config(' + JSON.stringify(fis_sea_conf, null, opt.optimize ? null : 4) + ');');
+	//构造nfejs的config.js配置文件
+	var nfejs_config = fis.file(fis.project.getProjectPath(), 'nfe-config.js');
+	//拼接字符串，生成nfe.setConfig调用
+	nfejs_config.setContent('nfe.setConfig(' + JSON.stringify(fis_nfejs_conf, null, opt.optimize ? null : 4) + ');');
 	//把新生成的文件放到打包文件输出表
-	ret.pkg[seajs_config.subpath] = seajs_config;
+	ret.pkg[nfejs_config.subpath] = nfejs_config;
 	//构造页面插入的script标签内容
-	var script = '<script src="' + seajs_config.getUrl(opt.hash, opt.domain) + '"></script>';
+	var script = '<script src="' + nfejs_config.getUrl(opt.hash, opt.domain) + '"></script>';
 	//找到所有的源码文件，对其进行配置文件script标签插入
 	fis.util.map(ret.src, function(subpath, file){
 		var id = file.getId();
@@ -32,7 +32,7 @@ module.exports = function(ret, conf, settings, opt){
 		}
 		if(file.isHtmlLike){
 			var cnt = file.getContent();
-			if(/\bseajs\.use\s*\(/.test(cnt)){//有用到seajs.use才插入配置
+			if(/\bnfe\.use\s*\(/.test(cnt)){//有用到nfe.use才插入配置
 				cnt = cnt.replace(/<\/head>/, script + '\n$&');
 				file.setContent(cnt);
 			}
